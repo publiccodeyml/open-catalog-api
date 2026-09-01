77240099fd5910539d51d0626002c77429abae87
Fabio Bonelli <fb@fabiobonelli.it>
2026-09-01 15:56:53 +0200

    test: add cache header assertions for /v1/status

--- status_test.go
+++ status_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHealthEndpoints(t *testing.T) {
@@ -23,9 +26,21 @@ func TestHealthEndpoints(t *testing.T) {
 			expectedCode:        204,
 			expectedBody:        "",
 			expectedContentType: "",
-			// TODO: test cache headers
 		},
 	}
 
 	runTestCases(t, tests)
+}
+
+func TestStatusCacheHeaders(t *testing.T) {
+	loadFixtures(t)
+
+	req, err := newTestRequest("GET", "/v1/status", nil)
+	require.NoError(t, err)
+
+	res, err := app.Test(req, -1)
+	require.NoError(t, err)
+
+	assert.Equal(t, 204, res.StatusCode)
+	assert.Equal(t, "no-cache", res.Header.Get("Cache-Control"))
 }
